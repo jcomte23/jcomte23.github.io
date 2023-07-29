e770eb738d096f77126ff682d5c5f4e19519210d
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-29 09:56:40 -0500

    mejoras en el  codigo

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 // JavaScript para el desplazamiento automático de la galería
-    const gallery = document.querySelector('.gallery');
-    // Clonar las imágenes para crear el efecto continuo
-    const images = gallery.querySelectorAll('.card');
+const gallery = document.querySelector('.gallery');
+// Clonar las imágenes para crear el efecto continuo
+const images = gallery.querySelectorAll('.card');
 
-    const totalImages = images.length;
-    for (let i = 0; i < totalImages; i++) {
-      gallery.appendChild(images[i].cloneNode(true));
-    }
-    // Establecer la duración de la animación
-    const animationDuration = 50; // Ajustar según la velocidad deseada
-    // Animación del desplazamiento horizontal
+const totalImages = images.length;
+for (let i = 0; i < totalImages; i++) {
+    gallery.appendChild(images[i].cloneNode(true));
+}
+// Establecer la duración de la animación
+const animationDuration = 15; // Ajustar según la velocidad deseada
+// Animación del desplazamiento horizontal
+// Verificar si es un dispositivo móvil (ancho de pantalla menor a 768 píxeles)
+if (window.innerWidth > 768) {
     gallery.style.animation = `slide ${animationDuration}s linear infinite`;
+}
